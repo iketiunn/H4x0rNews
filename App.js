652a86da00f89441c1d37dcc078f782c9b8382bd
ike <10249208+ikewat@users.noreply.github.com>
2019-08-30 11:26:34 +0800

    eject to bare react native project

--- App.js
+++
@@ -1,7 +0,0 @@
-/**
- * Do not modify this file - it is a proxy to your `App.re` file
- * located in the `src/` folder.
- *
- * There's too many way to make this work!
- */
-export { app as default } from './src/App.bs.js'
--- ios/h4x0rnews/AppDelegate.h
+++ ios/h4x0rnews/AppDelegate.h
@@ -1,8 +1,15 @@
-// Copyright 2015-present 650 Industries. All rights reserved.
+/**
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
 
+#import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
-#import <ExpoKit/EXStandaloneAppDelegate.h>
 
-@interface AppDelegate : EXStandaloneAppDelegate <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RCTBridgeDelegate>
+
+@property (nonatomic, strong) UIWindow *window;
 
 @end
